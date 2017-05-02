bce7b007cc0b2d1fcd811eab766b124be9296b8d
Jack Griffiths <Jack Griffiths>
2017-05-02 18:11:30 +0100

    Version 1.2: Road map completed, various layering issues fixed. Main Menu Added.

---
+++ LostCity/Assets/Scripts/CarAlarmBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarAlarmBehaviour : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip CarAlarm;
+
+    void OnTrigger2D(Trigger2D other)
+    {
+        if (other.gameObject.tag == "Bullet(Clone)")
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = CarAlarm;
+            audioSource.Play();
+        }
+    }
+}
---
+++ LostCity/Assets/Scripts/PlayButtonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Main");
+    } 
+    void OnMouseEnter()
+    {
+        transform.position = new Vector3(-1, 2, 1);
+    }
+
+    private void OnMouseExit()
+    {
+        transform.position = new Vector3(-1, 2, -1);
+    }
+}
