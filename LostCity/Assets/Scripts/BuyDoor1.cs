5184df359763c85f979e21c53846f25076c890be
Jack Griffiths <Jack Griffiths>
2017-05-04 02:04:26 +0100

    Version 1.2: Scripts started on buying doors and various map changes.

---
+++ LostCity/Assets/Scripts/BuyDoor1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyDoor1 : MonoBehaviour {
+
+    bool hasCollided = false;
+    string labelText = "";
+ 
+    void OnGUI()
+    {
+        if (hasCollided == true)
+        {
+            GUI.Box(new Rect(140, Screen.height - 50, Screen.width - 300, 120), (labelText));
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+
+        {
+
+            hasCollided = true;
+            labelText = "Hit F to move the barrier!";
+
+        }
+    }
+
+    void OnTriggerExit(Collider other )
+    {
+        hasCollided = false;
+
+    }
+
+}
