13de5411cbee8a6036cafe9ed332479e0a87d7bd
Jack Griffiths <Jack Griffiths>
2017-05-20 23:38:00 +0100

    Version 1.2 : Scroll Wheel now displays weapon change and Sprites created.

--- LostCity/Assets/Scripts/GameUI.cs
+++ LostCity/Assets/Scripts/GameUI.cs
@@ -6,12 +6,19 @@ public class GameUI : MonoBehaviour {
 
     private int health;
     private int score;
+    public static int WeaponIndex = 1;
     GUIContent content;
     GUIStyle style = new GUIStyle();
 
     private string gameInfo = "";
-    public  Texture BoxTexture;
+    public  Texture rifle;
+    public Texture shotgun;
+    public Texture automatic;
+    public Texture rocket;
+    public Texture pistol;
     private Rect boxRect = new Rect(10, 10, 300, 50);
+
+
 
     void OnEnable()
     {
@@ -23,10 +30,33 @@ public class GameUI : MonoBehaviour {
         PlayerBehaviour.OnUpdateHealth -= HandleonUpdateHealth;
         AddScore.OnSendScore -= HandleonSendScore;
     }
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            WeaponIndex += 1;
+            if (WeaponIndex == 5)
+            {
+                WeaponIndex = 1;
+            }
+            WeaponChangeBehaviour();
+
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            WeaponIndex -= 1;
+            if (WeaponIndex == 0)
+            {
+                WeaponIndex = 6;
+            }
+            WeaponChangeBehaviour();
+
+        }
+    }
     void Start()
     {
         UpdateUI();
-        content = new GUIContent("", BoxTexture, "This is a tooltip");
+        content = new GUIContent("", pistol, "This is a tooltip");
         style.alignment = TextAnchor.MiddleCenter;
         style.imagePosition = ImagePosition.ImageAbove;
     }
@@ -47,6 +77,34 @@ public class GameUI : MonoBehaviour {
     void OnGUI()
     {
         GUI.Box(boxRect, gameInfo);
-        GUI.Box(new Rect(50, Screen.height - 100, 200, 80), content, style);
+        GUI.Box(new Rect(10, Screen.height - 100, 200, 80), content, style);
     }
-}
+    public void WeaponChangeBehaviour()
+    {
+        if (WeaponIndex == 1)
+        {
+            //Pistol
+            content = new GUIContent("", pistol, "This is a tooltip");
+        }
+        if (WeaponIndex == 2)
+        {
+            //Shotgun
+            content = new GUIContent("", shotgun, "This is a tooltip");
+        }
+        if (WeaponIndex == 3)
+        {
+            //Automatic
+            content = new GUIContent("", automatic, "This is a tooltip");
+        }
+        if (WeaponIndex == 4)
+        {
+            //Rocket
+            content = new GUIContent("", rocket, "This is a tooltip");
+        }
+        if (WeaponIndex == 5)
+        
+            //Rifle
+            content = new GUIContent("", rifle, "This is a tooltip");
+    }
+    }
+
--- LostCity/Assets/Scripts/PlayerBehaviour.cs
+++ LostCity/Assets/Scripts/PlayerBehaviour.cs
@@ -8,9 +8,15 @@ public class PlayerBehaviour : MonoBehaviour {
     public delegate void UpdateHealth(int newHealth);
     public static event UpdateHealth OnUpdateHealth;
     public int health = 100;
-    public int WeaponIndex;
+
     private Animator gunAnim;
     private Sprite newSprite;
+
+    public Sprite Playerrifle;
+    public Sprite Playershotgun;
+    public Sprite Playerautomatic;
+    public Sprite Playerrocket;
+    public Sprite Playerpistol;
 
     void Start()
     {
@@ -19,26 +25,6 @@ public class PlayerBehaviour : MonoBehaviour {
     }
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
-        {
-            WeaponIndex += 1;
-            if (WeaponIndex == 6)
-            {
-                WeaponIndex = 1;
-            }
-            WeaponChangeBehaviour();
-            
-        }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
-        {
-            WeaponIndex -= 1;
-            if (WeaponIndex == 0)
-            {
-                WeaponIndex = 6;
-            }
-            WeaponChangeBehaviour();
-
-        }
         if (Input.GetMouseButtonDown(0))
         {
             GetComponent<AudioSource>().Play();
@@ -47,6 +33,27 @@ public class PlayerBehaviour : MonoBehaviour {
         if (Input.GetMouseButtonUp(0))
         {
             GetComponent<Animator>().SetBool("isFiring", false);
+        }
+
+        if (GameUI.WeaponIndex == 1)
+        {
+            GetComponent<SpriteRenderer>().sprite = Playerpistol;
+        }
+        else if (GameUI.WeaponIndex == 2)
+        {
+            GetComponent<SpriteRenderer>().sprite = Playershotgun;
+        }
+        else if (GameUI.WeaponIndex == 3)
+        {
+            GetComponent<SpriteRenderer>().sprite = Playerautomatic;
+        }
+        else if (GameUI.WeaponIndex == 4)
+        {
+            GetComponent<SpriteRenderer>().sprite = Playerrocket;
+        }
+        else if (GameUI.WeaponIndex == 5)
+        {
+            GetComponent<SpriteRenderer>().sprite = Playerrifle;
         }
     }
     public void TakeDamage(int damage)
@@ -66,34 +73,6 @@ public class PlayerBehaviour : MonoBehaviour {
         if (OnUpdateHealth != null)
         {
             OnUpdateHealth(health);
-        }
-    }
-    public void WeaponChangeBehaviour()
-    {
-
-        if (WeaponIndex== 1)
-        {
-
-        }
-        if (WeaponIndex == 2)
-        {
-
-        }
-        if (WeaponIndex == 3)
-        {
-
-        }
-        if (WeaponIndex == 4)
-        {
-
-        }
-        if (WeaponIndex == 5)
-        {
-
-        }
-        if (WeaponIndex == 6)
-        {
-
         }
     }
 }
